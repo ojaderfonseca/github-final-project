176b1d0bb5b770520da69473b1618c02997c2ee5
Jader Fonseca <13683137+ojaderfonseca@users.noreply.github.com>
2023-07-19 00:20:06 -0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <ojader>
+   # <ojaderfonseca>
    # Input:
    # p, principal amount
    # t, time period in years
